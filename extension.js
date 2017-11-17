7b9f470baafb6c1c89e83eb3aefa522639c2776d
ALM <ALM@dhcp-v105-036.mobile.uci.edu>
2017-11-16 18:52:13 -0800

    Made regex matches more specific

--- extension.js
+++ extension.js
@@ -8,21 +8,37 @@ const {
 } = require('vscode')
 const string = require("underscore.string");
 
+function isCapitalCase(word) {
+    return /^([A-Z]+[a-z]*)+[1-9]*/.test(word);
+  }
+  
+  function isSnakeCase(word) {
+    return /^([a-z]+_[a-z]+)+[1-9]*/.test(word);
+  }
+  
+  function isDashCase(word) {
+    return /^([a-z]+-[a-z]+)+[1-9]*/.test(word);
+  }
+  
+  function isCamelCase(word) {
+    return /^[a-z]+([A-Z]+[a-z]*)+[1-9]*/.test(word);
+  }
+
 exports.activate = () => {
     registerCommand('extension.stringConvert', () => {
         editor.edit(builder => {
             editor.selections.forEach(selection => {
                 let text = editor.document.getText(selection);
-                if (/^[A-Z]/.test(text)) {
+                if (isCapitalCase(text)) {
                     text = string.underscored(text);
-                } else if (/_/.test(text)) {
+                  } else if (isSnakeCase(text)) {
                     text = string.camelize(text, true);
-                } else if (/[A-Z]/.test(text)) {
+                  } else if (isCamelCase(text)) {
                     text = string.dasherize(text);
-                } else if (/-/.test(text)){
+                  } else if (isDashCase(text)) {
                     text = string.camelize(text);
                     text = string.capitalize(text);
-                }
+                  }
                 builder.replace(selection, text);
             });
         });
